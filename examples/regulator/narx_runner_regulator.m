eb6640cae8a9de9d1fbc3b0a004109ff390dfac6
Robin Otto <robin.otto@iteratec.de>
2017-07-14 10:28:43 +0200

    new nets using the midline data, one of them with regularization

--- examples/regulator/narx_runner_regulator.m
+++ examples/regulator/narx_runner_regulator.m
@@ -2,14 +2,12 @@ setupWorkspace;
 
 tic
 
-curCell = cell(1);
-singleResult = cell(1);
 xi = cell(1, 4);
 xi{1} = 0;
 xi{2} = 0;
 xi{3} = 0;
 xi{4} = 0;
-Y_new = neural_function(X, xi);
-plot(cell2mat(Y_new)); hold on; plot(Y); hold off;
+Y_nn_midline = neural_function_midline_reg(X, xi);
+plot(cell2mat(Y_nn_midline)); hold on; plot(Y); hold off;
 
 toc
--- examples/regulator/setupWorkspace.m
+++ examples/regulator/setupWorkspace.m
@@ -6,7 +6,7 @@ stepSize = 1; % stepsize to take in cmerr_to_cmangle_rad2deg.csv
 
 tic
 
-csv_data_cm = csvread('cmerr_to_cmangle_rad2deg.csv', 2, 0);
+csv_data_cm = csvread('cmerr_to_cmangle_rad2deg_v4.csv', 2, 0);
 % csv_data_rg = csvread('data_wemding_regulator.csv', 2, 0);
 
 X_CM = csv_data_cm(from:stepSize:to, 1:3);
